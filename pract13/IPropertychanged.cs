762a75cd47d3ed55fe909861f88f5843b22aaebf
Abai <abai2010@bk.ru>
2017-12-19 19:30:45 +0600

    finished

---
+++ pract13/IPropertychanged.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract13
+{
+    public delegate void PropertyEventHandler(object sender, string args);
+    public interface IPropertyChanged
+    {
+        event PropertyEventHandler PropertyChanged;
+    }
+}
---
+++ pract13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person() { Name = "Alex", Age = 29 };
+
+            person1.PropertyChanged += new ConsoleSender().PrintMessage;
+            person1.Name = "Petr";
+            person1.Age = 29;
+            
+            Console.ReadLine();
+        }
+    }
+}
